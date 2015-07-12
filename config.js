b8fd64ae4b5c2805f13ed06ffec5d816f4fc077c
Mehdi-H <mehdi.houacine@edu.esiee.fr>
2015-07-12 18:36:51 +0200

    Création d'un fichier config.js pour y placer quelques paramètres. Ajout de middleware de parsing et de logging à server.js.

---
+++ config.js
@@ -0,0 +1,15 @@
+/* 
+* @Author: Mehdi-H
+* @Date:   2015-07-12 18:23:00
+* @Last Modified by:   Mehdi-H
+* @Last Modified time: 2015-07-12 18:25:18
+*/
+
+/*jslint node: true */
+'use strict';
+
+module.exports = {
+	"database": "",
+	"port": process.env.PORT || 3000,
+	"secretKey": "MySecretKey"
+};
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: Mehdi-H
 * @Date:   2015-07-12 16:35:42
 * @Last Modified by:   Mehdi-H
-* @Last Modified time: 2015-07-12 16:47:43
+* @Last Modified time: 2015-07-12 18:34:25
 */
 
 /*jslint node: true */
@@ -11,15 +11,21 @@
 /*==========  Pkg node.js  ==========*/
 var express    = require('express'),
 	bodyParser = require('body-parser'),
-	morgan     = require('morgan');
+	morgan     = require('morgan'),
+	config     = require('./config');  // fichier config
+
+/*==========  Middleware  ==========*/
+app.use(bodyParser.urlencoded({extended:true}));  // false ne permet que de parser une string, true pour toute sorte de médias.
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 /*==========  Instanciation  ==========*/
 var app = express();
 
 /*==========  diffusion  ==========*/
-app.listen(3000, function(err){
+app.listen(config.port, function(err){
 	if(err)
 		console.log('Erreur: ' + err);
 	else
-		console.log('L\'application tourne sur le port 3000');
+		console.log('L\'application tourne sur le port ' + config.port);
 });
