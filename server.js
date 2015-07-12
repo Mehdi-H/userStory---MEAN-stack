29c48c1c491d4dc83f9004bca444837d3b21df24
Mehdi-H <mehdi.houacine@edu.esiee.fr>
2015-07-12 18:50:17 +0200

    Ajout du fichier index.html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 * @Author: Mehdi-H
 * @Date:   2015-07-12 16:35:42
 * @Last Modified by:   Mehdi-H
-* @Last Modified time: 2015-07-12 18:34:25
+* @Last Modified time: 2015-07-12 18:41:21
 */
 
 /*jslint node: true */
@@ -14,13 +14,18 @@ var express    = require('express'),
 	morgan     = require('morgan'),
 	config     = require('./config');  // fichier config
 
+/*==========  Instanciation  ==========*/
+var app = express();
+
 /*==========  Middleware  ==========*/
 app.use(bodyParser.urlencoded({extended:true}));  // false ne permet que de parser une string, true pour toute sorte de médias.
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-/*==========  Instanciation  ==========*/
-var app = express();
+/*==========  Content  ==========*/
+app.get('*', function(req,res){
+	res.sendFile(__dirname + '/public/views/index.html');
+});
 
 /*==========  diffusion  ==========*/
 app.listen(config.port, function(err){
