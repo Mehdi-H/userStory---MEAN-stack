88b29131bf5e4fd766e0917674fd2a05e56778d1
Mehdi-H <mehdi.houacine@edu.esiee.fr>
2015-07-13 15:38:53 +0200

    Possibilité de se connecter et de conserver la connexion avec un token hashé grace au package jsonwebtoken

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,7 +2,7 @@
 * @Author: Mehdi-H
 * @Date:   2015-07-12 21:01:07
 * @Last Modified by:   Mehdi-H
-* @Last Modified time: 2015-07-13 15:17:48
+* @Last Modified time: 2015-07-13 15:35:51
 */
 
 /*jslint node: true */
@@ -10,16 +10,31 @@
 
 /*==========  Pkg nodeJS  ==========*/
 
-var User      = require('../models/user'),
-	config    = require('../../config'),
-	secretKey = config.secretKey;
+var User         = require('../models/user'),
+	config       = require('../../config'),
+	secretKey    = config.secretKey,
+	jsonWebToken = require('jsonwebtoken');
+
+
+function createToken(user){
+	var token = jsonWebToken.sign({
+		_id: user._id,
+		name: user.name,
+		username: user.username
+	}, secretKey, {
+		expiresInMinute: 1440
+	});
+
+	return token;
+}
+
 
 module.exports = function(app, express){
 
-	// router d'express
+	/*==========  router d'express  ==========*/
 	var api = express.Router();
 
-	// POST
+	/*==========  POST  ==========*/	
 	// récupération des données d'inscription
 	// et sauvegarde du nouvel utilisateur
 	api.post('/signup', function(req,res){
@@ -29,7 +44,7 @@ module.exports = function(app, express){
 			password: req.body.password
 		});
 
-		// sauvegarde des données
+		// Sauvegarde des données
 		user.save(function(err){
 			if(err){
 				res.send('Erreur :' + err);
@@ -40,7 +55,7 @@ module.exports = function(app, express){
 		});
 	});	
 
-	// GET
+ 	/*==========  GET  ==========*/ 
 	// affichage de tous les utilisateurs
 	api.get('/users',function(req,res){
 		User.find({}, function(err,users){
@@ -49,6 +64,32 @@ module.exports = function(app, express){
 				return;
 			}else{
 				res.json(users);
+			}
+		});
+	});
+
+
+	api.post('/login', function(req,res){
+		User.findOne({
+			username: req.body.username
+		}).select('password').exec(function(err,user){
+			if(err){
+				throw err;
+			}else if(!user){
+				res.send('Cet utilisateur n\'existe pas.');
+			}else{
+				var validPassword = user.comparePassword(req.body.password);
+				if(!validPassword){
+					res.send({message: 'Mot de passe invalide'});
+				}else{
+					// création d'un token
+					var token = createToken(user);
+					res.json({
+						success: true,
+						message: 'La connexion a réussi!',
+						token: token
+					}); // test: axyom,root
+				}
 			}
 		});
 	});
